d395020cac534a55c3e158bb99261c04242d6845
alexkozac <alessandrokozac@gmail.com>
2023-12-20 10:45:34 +0100

    1

--- AMA.py
+++ AMA.py
@@ -1,4 +1,9 @@
 import pandas as pd
-url = ''
+url = 'https://raw.githubusercontent.com/usnmweb/python-AMA/main/csv/filmtv_movies.csv'
 df = pd.read_csv(url)
 df.columns
+
+
+avg_vote_title = df['avg_vote'].idxmax()
+avg_title_max = df.loc[avg_vote_title, "title"]
+print("il voto medio piu alto di un film è di:",avg_title_max )
