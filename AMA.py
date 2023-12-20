4495d8c2b5bd0a005e2f7991c3e482ac8f959a08
alexkozac <alessandrokozac@gmail.com>
2023-12-20 12:12:44 +0100

    1.2

--- AMA.py
+++ AMA.py
@@ -12,3 +12,20 @@ print(df.shape)
 top_five = df.sort_values(by='avg_vote', ascending=False).head(5)
 list_of_columns = ["title" ,"year", "genre"]
 print("la top 5 con il voto medio piu alto sono:",top_five[list_of_columns])
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+short_df = top_five
+
+x = short_df["year"]
+y = short_df["avg_vote"]
+
+plt.bar(x, y)
+
+
+
+plt.ylim(9, 10.1)
+
+plt.show()
